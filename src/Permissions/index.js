7ee1fbeb22750fb5faee4f1977dcddc734c9815c
Alexandr Oleynichenko <oleynichenkos@gmail.com>
2023-05-11 15:31:24 +0300

    fix styles related with react-navigation updates

--- src/Permissions/index.js
+++ src/Permissions/index.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
+import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import HeaderComponent from '../Header/index';
-import { StyleSheet, Text, View, Platform, Image, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
   icons: {
     width: 40,
     height: 40,
-    marginRight: '5%'
-  },
-
-  iosTitle: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginVertical: '5%',
-    fontWeight: 'bold'
-  },
-  iosSubtitle: {
-    textAlign: 'center',
-    fontSize: 16,
-    marginBottom: '5%',
-    marginLeft: '5%',
-    marginRight: '5%',
-    color: '#919191'
+    marginRight: 15
   },
   step: {
     fontSize: 14,
     flexDirection: 'row',
-    marginBottom: '5%',
+    marginBottom: 15,
     alignItems: 'center'
-  },
-  steps: {
-    flexDirection: 'column'
   }
 });
 
@@ -43,11 +25,15 @@ export default class PermissionsComponent extends Component {
     return Platform.select({
       ios: (
         <View style={{ alignItems: 'center' }}>
-          <View style={styles.steps}>
+          <View>
             {text.iosSteps.map(step => (
               <View style={styles.step} key={step.label}>
                 <Image source={step.img} style={styles.icons} />
-                <Text>{step.label}</Text>
+                <Text
+                  style={{ fontSize: 16, color: '#505050' }}
+                >
+                  {step.label}
+                </Text>
               </View>
             ))}
           </View>
@@ -55,11 +41,11 @@ export default class PermissionsComponent extends Component {
       ),
       android: (
         <View style={{ alignItems: 'center' }}>
-          <View style={styles.steps}>
+          <View>
             {text.androidSteps.map(step => (
               <View style={styles.step} key={step.label}>
                 <Image source={step.img} style={styles.icons} />
-                <Text>{step.label}</Text>
+                <Text style={{ fontSize: 16, color: '#505050' }}> {step.label}</Text>
               </View>
             ))}
           </View>
@@ -70,12 +56,38 @@ export default class PermissionsComponent extends Component {
 
   render() {
     const { text, primaryColor } = this.props;
+
     return (
-      <View>
+      <View style={{ flexGrow: 1, backgroundColor: 'white' }}>
         <HeaderComponent title={text.headerTitle} backgroundColor={primaryColor} />
-        <Text style={styles.iosTitle}>{text.title}</Text>
-        <Text style={styles.iosSubtitle}>{text.subTitle}</Text>
-        {this.platformRendering(text)}
+        <View
+          style={{
+            flexGrow: 1,
+            paddingHorizontal: 15,
+            marginTop: 50,
+          }}
+        >
+          <Text
+            style={{
+              marginBottom: 10,
+              fontSize: 20,
+              textAlign: 'center',
+              color: primaryColor,
+            }}
+          >
+            {text.title}
+          </Text>
+          <Text
+            style={{
+              marginBottom: 30,
+              textAlign: 'center',
+              color: '#919191'
+            }}
+          >
+            {text.subTitle}
+          </Text>
+          {this.platformRendering(text)}
+        </View>
       </View>
     );
   }
